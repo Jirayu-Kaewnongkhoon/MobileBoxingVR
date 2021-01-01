c431f237102017f02bf64b360681c6a51fcc4c8c
JirayuKaewnongkhoon <60050149@kmitl.ac.th>
2021-01-02 00:09:13 +0700

    Add Simple Register function

--- app/src/main/java/com/app/mobileboxingvr/register/Register.java
+++ app/src/main/java/com/app/mobileboxingvr/register/Register.java
@@ -1,16 +1,79 @@
 package com.app.mobileboxingvr.register;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.app.mobileboxingvr.MainActivity;
 import com.app.mobileboxingvr.R;
+import com.app.mobileboxingvr.services.UserService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class Register extends AppCompatActivity {
+
+    private UserService user;
+
+    private EditText username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeView();
+    }
+
+    private void initializeView() {
+        username = findViewById(R.id.etUsername);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+
+        user = UserService.getInstance();
+    }
+
+    public void onRegisterClick(View view) {
+        user.register(email.getText().toString().trim(), password.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            updateProfile();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "Registration failed", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    public void updateProfile() {
+        user.updateProfile(username.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Log.d("REGISTER", "updateProfile: " + user.getCurrentUser().getDisplayName());
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+
+                        }
+
+                    }
+                });
     }
 }
