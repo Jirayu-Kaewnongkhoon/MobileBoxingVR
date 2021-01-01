19799d6e87fc71074d7e2f8c74f08fb7236148c5
JirayuKaewnongkhoon <60050149@kmitl.ac.th>
2021-01-01 15:36:27 +0700

    Initial commit

---
+++ app/src/main/java/com/app/mobileboxingvr/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.app.mobileboxingvr.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.mobileboxingvr.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
